6fa41e8a7e64763c603dc49e79335841edfb1729
Arjurr <arne.jurr@itech-bs14.de>
2026-01-26 13:30:18 +0100

    Landing Page angepasst

--- View/app.py
+++ View/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Fußball App",
+    page_title="Fußball Datenbank IuK II",
     layout="wide"
 )
 
-st.title("Fußball App")
+st.title("Fußball Datenbank IuK II")
